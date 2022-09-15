fc17a06c6da71abbbf7135691d1214599b6b60dc
crystal <crystalokd@gmail.com>
2022-09-15 08:17:24 +0100

    ...

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,34 @@
+export const Questions = [
+    {
+        prompt: "What is 21 + 3",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer: "A"
+    },
+    {
+        prompt: "What is 1 + 3",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer: "C"
+    },
+    {
+        prompt: "What is 18 + 3",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer: "B"
+    },
+    {
+        prompt: "What is 1 + 11",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "12",
+        answer: "D"
+    },
+]
